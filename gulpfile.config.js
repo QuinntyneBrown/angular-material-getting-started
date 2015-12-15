5693fd8105960bb72ea4bcbfbeb40fc02166fbfe
Quinntyne Brown <quinntynebrown@gmail.com>
2015-12-15 04:15:48 -0500

    initial code commit

---
+++ gulpfile.config.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+var GulpConfig = (function () {
+
+    function GulpConfig() {
+        this.source = './wwwroot/';
+        this.allJavaScript = this.source + '/**/*.js';
+        this.allFiles = this.source + '/**/*.js';
+
+        this.paths = {
+            npm: './node_modules/',
+            bower: './bower_components/',
+            lib: './lib/'
+        };
+
+        this.libs = [
+            this.paths.bower + 'angular-material/angular-material.js',
+            this.paths.npm + 'angular/angular.js',
+            this.paths.npm + 'angular-aria/angular-aria.js',
+            this.paths.npm + 'angular-animate/angular-animate.js',
+            this.paths.npm + 'angular-route/angular-route.js',
+            this.paths.npm + 'jquery/dist/jquery.js',
+            this.paths.npm + 'ng-x/dist/ngX.js'
+        ];
+    }
+
+    return GulpConfig;
+})();
+
+module.exports = GulpConfig;
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var gulp = require("gulp");
+var concat = require('gulp-concat');
+var rimraf = require('rimraf');
+
+var Config = require('./gulpfile.config');
+
+var config = new Config();
+
+gulp.task('concat-js', function () {
+    return gulp.src(config.allJavaScript)
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', ['concat-js'], function () {
+    gulp.watch(config.allFiles, ['concat-js']);
+});
+
+gulp.task('libs', function () {
+    return gulp.src(config.libs).pipe(gulp.dest(config.paths.lib));
+});
+
+gulp.task('clean', function (callback) {
+    rimraf(config.paths.lib, callback);
+});
+
+
+gulp.task('default', ['libs', 'concat-js', 'watch']);
